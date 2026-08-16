65ff483a95709e91994f7fe318ad666bf960e979
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-08-16 03:55:19 -0300

    chore(generated): regenerate lazy package initializers

--- src/flext_tests/__init__.py
+++ src/flext_tests/__init__.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from types import MappingProxyType
+
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 from .__version__ import __author__ as __author__
@@ -32,32 +34,6 @@ if TYPE_CHECKING:
     from .typings import FlextTestsTypes, FlextTestsTypes as t
     from .utilities import FlextTestsUtilities, FlextTestsUtilities as u
     from .validator import FlextTestsValidator, tv
-
-_LAZY_MODULES: dict[str, tuple[str, ...]] = {
-    "._config": ("FlextTestsConfig", "config"),
-    "._settings": ("FlextTestsSettings", "settings"),
-    ".base": ("FlextTestsCase", "FlextTestsServiceBase", "s"),
-    ".constants": ("FlextTestsConstants", "c"),
-    ".docker": ("FlextTestsDocker", "tk"),
-    ".domains": ("FlextTestsDomains", "td"),
-    ".files": ("FlextTestsFiles", "tf"),
-    ".models": ("FlextTestsModels", "m"),
-    ".protocols": ("FlextTestsProtocols", "p"),
-    ".tmatchers": ("FlextTestsMatchersUtilities", "tm"),
-    ".typings": ("FlextTestsTypes", "t"),
-    ".utilities": ("FlextTestsUtilities", "u"),
-    ".validator": ("FlextTestsValidator", "tv"),
-    "flext_infra": ("d", "e", "h", "r", "x"),
-}
-
-
-_LAZY_ALIAS_GROUPS: dict[str, tuple[tuple[str, str], ...]] = {}
-
-
-_LAZY_IMPORTS = build_lazy_import_map(
-    _LAZY_MODULES, alias_groups=_LAZY_ALIAS_GROUPS, sort_keys=False
-)
-
 __all__: tuple[str, ...] = (
     "FlextTestsCase",
     "FlextTestsConfig",
@@ -101,4 +77,30 @@ __all__: tuple[str, ...] = (
     "x",
 )
 
-install_lazy_exports(__name__, globals(), _LAZY_IMPORTS, public_exports=__all__)
+install_lazy_exports(
+    __name__,
+    globals(),
+    MappingProxyType(
+        build_lazy_import_map(
+            MappingProxyType({
+                "._config": ("FlextTestsConfig", "config"),
+                "._settings": ("FlextTestsSettings", "settings"),
+                ".base": ("FlextTestsCase", "FlextTestsServiceBase", "s"),
+                ".constants": ("FlextTestsConstants", "c"),
+                ".docker": ("FlextTestsDocker", "tk"),
+                ".domains": ("FlextTestsDomains", "td"),
+                ".files": ("FlextTestsFiles", "tf"),
+                ".models": ("FlextTestsModels", "m"),
+                ".protocols": ("FlextTestsProtocols", "p"),
+                ".tmatchers": ("FlextTestsMatchersUtilities", "tm"),
+                ".typings": ("FlextTestsTypes", "t"),
+                ".utilities": ("FlextTestsUtilities", "u"),
+                ".validator": ("FlextTestsValidator", "tv"),
+                "flext_infra": ("d", "e", "h", "r", "x"),
+            }),
+            alias_groups=MappingProxyType({}),
+            sort_keys=False,
+        )
+    ),
+    public_exports=__all__,
+)
