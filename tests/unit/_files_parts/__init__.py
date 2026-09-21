e44ef1083fecfd6350e03755fbb549e81802c1ff
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 14:21:44 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- tests/unit/_files_parts/__init__.py
+++ tests/unit/_files_parts/__init__.py
@@ -9,9 +9,7 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_tests import c, d, e, h, p, r, s, t, td, tf, tk, tm, tv, u, x
-
-    from .models import m
+    from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
 __all__: tuple[str, ...] = (
     "c",
     "d",
@@ -34,12 +32,12 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".models": ("m",),
             "flext_tests": (
                 "c",
                 "d",
                 "e",
                 "h",
+                "m",
                 "p",
                 "r",
                 "s",
